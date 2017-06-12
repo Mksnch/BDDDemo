dfa72f2586f0581700945f225c124fd94acb6e3d
Mksnch <maksabox@gmail.com>
2017-06-12 13:44:57 +0300

    test repo for bdddemo

---
+++ BDDBasketDemo/Class1.cs
@@ -0,0 +1,24 @@
+﻿using TechTalk.SpecFlow;
+
+namespace BDDBasketDemo
+{
+    [Binding]
+    public class StepDefinitions
+    {
+        [When(@"I create a new basket")]
+        public void WhenICreateANewBasket()
+        {
+            var basket = new Basket();
+        }
+
+        [Then(@"the basket has no content")]
+        public void ThenTheBasketHasNoContent()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+
+    public class Basket
+        {
+        }
+    }
---
+++ BDDBasketDemo/ShoppingBasket.feature
@@ -0,0 +1,7 @@
+﻿Feature: ShoppingBasket
+	A simple shopping basket that we can add and remove items from
+
+@mytag
+Scenario: A new basket no content
+	When I create a new basket
+	Then the basket has no content
